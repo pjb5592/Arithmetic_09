5577f297d043723df9118174aa4d2cc4e84af4af
pjb5592 <pjb5592@gmail.com>
2026-05-12 15:47:11 +0900

    Test 완료

--- test/ArithmeticOperationsTest.cpp
+++ test/ArithmeticOperationsTest.cpp
@@ -8,29 +8,29 @@ void testAddition() {
   std::cout << "testAddition passed" << std::endl;
 }
 
-// void testSubtraction() {
-//     ArithmeticOperations ao;
-//     assert(ao.subtraction(4, 2) == 2);
-//     std::cout << "testSubtraction passed" << std::endl;
-// }
+void testSubtraction() {
+  ArithmeticOperations ao;
+  assert(ao.subtraction(4, 2) == 2);
+  std::cout << "testSubtraction passed" << std::endl;
+}
 
-// void testMultiplication() {
-//     ArithmeticOperations ao;
-//     assert(ao.multiplication(4, 2) == 8);
-//     std::cout << "testMultiplication passed" << std::endl;
-// }
+void testMultiplication() {
+  ArithmeticOperations ao;
+  assert(ao.multiplication(4, 2) == 8);
+  std::cout << "testMultiplication passed" << std::endl;
+}
 
-// void testDivision() {
-//     ArithmeticOperations ao;
-//     assert(ao.division(4, 2) == 2);
-//     std::cout << "testDivision passed" << std::endl;
-// }
+void testDivision() {
+  ArithmeticOperations ao;
+  assert(ao.division(4, 2) == 2);
+  std::cout << "testDivision passed" << std::endl;
+}
 
 int main() {
   testAddition();
-  //   testSubtraction();
-  //   testMultiplication();
-  //   testDivision();
+  testSubtraction();
+  testMultiplication();
+  testDivision();
   std::cout << "All tests passed!" << std::endl;
   return 0;
 }
