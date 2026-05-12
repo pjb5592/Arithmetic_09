2688294fa91999c2ecce1a6b6e3b23d79449ed3c
pjb5592 <pjb5592@gmail.com>
2026-05-12 13:33:50 +0900

    .gitignore 추가

--- src/ArithmeticOperations.cpp
+++ src/ArithmeticOperations.cpp
@@ -1,21 +1,11 @@
 #include "ArithmeticOperations.h"
 
-int ArithmeticOperations::addition(int x, int y) {
-    return x + y;
-}
+int ArithmeticOperations::addition(int x, int y) { return x + y; }
 
-int ArithmeticOperations::subtraction(int x, int y) {
-    return x - y;
-}
+int ArithmeticOperations::subtraction(int x, int y) { return x - y; }
 
-int ArithmeticOperations::multiplication(int x, int y) {
-    return x * y;
-}
+int ArithmeticOperations::multiplication(int x, int y) { return x * y; }
 
-int ArithmeticOperations::division(int x, int y) {
-    return x / y;
-}
+int ArithmeticOperations::division(int x, int y) { return x / y; }
 
-float ArithmeticOperations::quotient(int x, int y) {
-    return 0;
-}
+float ArithmeticOperations::quotient(int x, int y) { return 0; }
